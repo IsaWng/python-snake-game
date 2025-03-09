064ec5f87dcfc6a9a21866fca4dd9d054094c3b3
isa <qr-1124@outlook.com>
2025-03-09 20:04:22 +0100

    feature: control snake.

---
+++ snake.py
@@ -0,0 +1,47 @@
+from turtle import Turtle
+
+DISTANCE = 20
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for pos in STARTING_POSITION:
+            seg = Turtle("square")
+            seg.color("white")
+            seg.penup()
+            seg.goto(pos)
+            self.segments.append(seg)
+
+    def move(self):
+        for index in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[index - 1].xcor()
+            new_y = self.segments[index - 1].ycor()
+            self.segments[index].goto(new_x, new_y)
+        self.segments[0].forward(DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
